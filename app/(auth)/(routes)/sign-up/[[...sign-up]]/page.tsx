50b083933df669bad29c510913cf4b5308db99eb
samatbelentbay <belentbais03@mail.ru>
2026-01-17 15:23:13 +0500

    Update configuration and dependencies for Uploadthing integration
    - Modified Next.js configuration to support new image domains and remote patterns.
    - Upgraded Uploadthing and related packages to version 7.0.0, ensuring compatibility with the latest features.
    - Refactored upload components to utilize new Uploadthing methods for better functionality.
    - Updated sign-in and sign-up components to use fallback redirect URLs for improved user experience.

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <SignUp 
       afterSignUpUrl="/search"
-      afterSignInUrl="/search"
+      fallbackRedirectUrl="/search"
     />
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["utfs.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.ufs.sh",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+    ],
   },
 };
 
